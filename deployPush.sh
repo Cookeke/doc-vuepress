f06c236dc8935d7d19239f3028311b8480addbda
孔科翰 <kongkehan@jd.com>
2022-06-07 15:56:56 +0800

    update:docs

--- deployPush.sh
+++ deployPush.sh
@@ -4,24 +4,25 @@
 set -e
 
 # 生成静态文件
-yarn docs:build
+# yarn docs:build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+# cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
+# git init
 git add .
-git commit -m 'deploy'
+git commit -m 'update:docs'
+git push
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git remote add origin git@github.com:Cookeke/doc-vuepress.git
-git branch -M master
-git push -f git@github.com:Cookeke/doc-vuepress.git master:gh-pages
+# git remote add origin git@github.com:Cookeke/doc-vuepress.git
+# git branch -M master
+# git push -f git@github.com:Cookeke/doc-vuepress.git main:gh-pages
 
 cd -
