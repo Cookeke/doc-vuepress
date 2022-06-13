447fd767572f51135c69322a0e6ef1b0ed9a079e
孔科翰 <kongkehan@jd.com>
2022-06-13 15:39:11 +0800

    update:docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
     lang: 'zh-CN',
-    title: 'hello world2!',
-    description: 'this is my first vue-press site',
-    base: '/doc-vuepress/'
+    base: '/doc-vuepress/',
+    title: '科科的知识库',
+    description: '科科的知识库',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '科科的掘金', link: 'https://juejin.cn/user/3526889033186839/posts/' },
+            { text: '科科的Github', link: 'https://github.com/Cookeke/' },
+        ]
+    },
 }
